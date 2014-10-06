0e04d0a1b04d886afeeebe694a4928298802c6c1
markosharko <aemarkovich@gmail.com>
2014-10-06 08:38:31 -0400

    Save Handler

--- AEM-Game/src/alex/markovich/game/save/SaveHandler.java
+++ AEM-Game/src/alex/markovich/game/save/SaveHandler.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class SaveHandler {
 	private String filePath = "./Data/Save.txt";
@@ -22,6 +23,7 @@ public class SaveHandler {
 		}    
 		 printW = new PrintWriter(writer);
 		checkFilePath();
+		System.out.println(getName());
 	}
 	
 	public void checkFilePath(){
@@ -38,6 +40,30 @@ public class SaveHandler {
 				System.out.println("Shit something went wrong here; Line 18");
 			}
 		}
+	}
+	
+	public boolean DoesFileExist(){
+		if(f.exists()){
+			return true;
+		}else
+			return false;
+	}
+	
+	public String getName(){
+		Scanner x = null;
+		String s = null;
+		try{
+			x = new Scanner(new File(filePath));
+		}catch (IOException e){
+			System.out.println("Shit something went wrong.");
+		}
+		while(x.hasNext()){
+			 s = x.next();
+			 System.out.println(s);
+			 return s;
+		}
+		return s;
+		
 	}
 	
 	public void save(int Health, String Name){
